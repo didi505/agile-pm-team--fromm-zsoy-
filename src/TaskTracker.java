c8a069c5579a45a93a2580d856bf26f940a4d43a
milo160 <Milana.fromm@student.hs-rm.de>
2026-05-03 17:32:02 +0200

    Add project structure and sprint overview to TaskTracker
    Added project structure and sprint overview documentation.

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,1 +1,9 @@
+## Projektstruktur
+- src/: enthält den Java-Code
+- docs/: enthält Dokumentation wie User Stories und Rollen
+- abgaben/: enthält Abgaben der Wochen
 
+## Sprint-Übersicht
+| Sprint | Zeitraum | Ziel |
+|--------|----------|------|
+| 1 | Woche 2–3 | Basis-Funktionalität |
