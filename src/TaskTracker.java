b31a8a9038df5b38082e9ce896a27d9f67eed0f9
milo160 <Milana.fromm@student.hs-rm.de>
2026-05-03 18:07:14 +0200

    Update TaskTracker.java

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,9 +1,5 @@
-## Projektstruktur
-- src/: enthält den Java-Code
-- docs/: enthält Dokumentation wie User Stories und Rollen
-- abgaben/: enthält Abgaben der Wochen
-
-## Sprint-Übersicht
-| Sprint | Zeitraum | Ziel |
-|--------|----------|------|
-| 1 | Woche 2–3 | Basis-Funktionalität |
+public class TaskTracker {
+    public static void main(String[] args) {
+        System.out.println("Task Tracker läuft!");
+    }
+}
